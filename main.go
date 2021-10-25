703e8f98051d937557708ed74fe7125d580374a4
Parham Alvani <parham.alvani@gmail.com>
2021-10-25 20:46:17 +0000

    feat: update rfid reader

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ import (
 	"periph.io/x/host/v3/rpi"
 )
 
-const ReadTimeout = 5 * time.Minute
+const (
+	ReadTimeout = 5 * time.Minute
+	Gain        = 5
+)
 
 func main() {
 	if err := pterm.DefaultBigText.WithLetters(
@@ -49,7 +52,11 @@ func main() {
 	}
 
 	// setting the antenna signal strength, signal strength from 0 to 7
-	rfid.SetAntennaGain(5)
+	if err := rfid.SetAntennaGain(Gain); err != nil {
+		pterm.Error.Printf("antenna gain setup failed %s\n", err)
+
+		return
+	}
 
 	pterm.Info.Println("Started rfid reader.")
 
@@ -57,7 +64,9 @@ func main() {
 		// trying to read UID
 		data, err := rfid.ReadUID(ReadTimeout)
 		if err != nil {
-			pterm.Error.Printf("cannot read the rfid %s\n", err)
+			// here we ignore the reader error because of its many failures.
+			// pterm.Error.Printf("cannot read the rfid %s\n", err)
+			_ = err
 		} else {
 			pterm.Info.Println(hex.EncodeToString(data))
 		}
